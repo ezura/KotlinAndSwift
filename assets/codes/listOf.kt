1af3f9e669be4b9aec6a1046d0231162b200b504
Yuka Ezura <ezura@users.noreply.github.com>
2017-07-08 00:18:05 +0900

    Update listOf.kt

--- assets/codes/listOf.kt
+++ assets/codes/listOf.kt
@@ -1,8 +1,8 @@
 /** Returns a new read-only list of given elements. */
 fun <T> listOf(vararg elements: T): List<T> {
-  if (elements.size > 0) {
-    elements.asList() 
-  } else {
-    emptyList()
-  }
+    if (elements.size > 0) {
+        elements.asList() 
+    } else {
+        emptyList()
+    }
 }
